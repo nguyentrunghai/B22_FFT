395a81bb4fe710f51e52098add1b4b2b3d656f78
Trung Hai Nguyen <nguyentrungh@gmail.com>
2018-09-11 13:34:26 -0500

    add scripts/run_md.py

--- scripts/run_md.py
+++ scripts/run_md.py
@@ -3,11 +3,10 @@ run md simulation using OpenMM
 """
 from __future__ import print_function
 
-import os
 import sys
 import argparse
 
-sys.path.append("../b22_fft")
+sys.path.append("/home/nguyen76/opt/src/B22_FFT/b22_fft")
 from md_openmm import OpenMM_MD
 
 parser = argparse.ArgumentParser()
@@ -18,21 +17,26 @@ parser.add_argument( "--phase",                     type=str, default = "OpenMM_
 
 parser.add_argument( "--steps_per_iteration",       type=int, default = 500)
 parser.add_argument( "--niterations",               type=int, default = 1000)
-parser.add_argument( "--rotations_per_iteration",   type=int, default = 1)
 
 parser.add_argument( "--temperature",           type=float, default = 300.)
 
-parser.add_argument( "--out_prefix",           type=float, default = "traj")
+parser.add_argument( "--nc_out",           type=str, default = "traj.nc")
+parser.add_argument( "--inpcrd_prefix",    type=str, default = "none")
+parser.add_argument( "--pdb_out",          type=str, default = "none")
 
 args = parser.parse_args()
 
 md = OpenMM_MD(args.prmtop_file, args.inpcrd_file, phase=args.phase, temperature=args.temperature)
 
-nc_file_name = args.out_prefix + ".nc"
-inpcrd_prefix = args.out_prefix
-pdb_out = args.out_prefix + ".pdb"
+inpcrd_prefix = args.inpcrd_prefix
+if inpcrd_prefix.lower() == "none":
+       inpcrd_prefix = None
 
-md.run(nc_file_name, steps_per_iteration=args.steps_per_iteration, niterations=args.niterations,
+pdb_out = args.pdb_out
+if pdb_out.lower() == "none":
+       pdb_out = None
+
+md.run(args.nc_out, steps_per_iteration=args.steps_per_iteration, niterations=args.niterations,
        inpcrd_prefix=inpcrd_prefix, pdb_out=pdb_out)
 
 print("Done")
