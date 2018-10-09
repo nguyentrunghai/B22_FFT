a0cd1e6e084257b2754353a404c7f09262737f24
Trung Hai Nguyen <nguyentrungh@gmail.com>
2018-10-09 09:40:56 -0500

    modify scripts/run_pot_grid.py

--- scripts/run_pot_grid.py
+++ scripts/run_pot_grid.py
@@ -15,9 +15,6 @@ parser.add_argument( "--prmtop_file", type=str, default="protein.prmtop")
 parser.add_argument( "--inpcrd_file", type=str, default="protein.inpcrd")
 parser.add_argument( "--grid_nc",     type=str, default="grid.nc")
 
-parser.add_argument( "--lj_sigma_scaling_factor",     type=float, default=1.0)
-parser.add_argument( "--lj_depth_scaling_factor",     type=float, default=1.0)
-
 parser.add_argument( "--ionic_strength",     type=float, default=0.)
 parser.add_argument( "--dielectric",     type=float, default=80.)
 
@@ -28,8 +25,6 @@ parser.add_argument( "--x_count",     type=int, default=100)
 parser.add_argument( "--y_count",     type=int, default=100)
 parser.add_argument( "--z_count",     type=int, default=100)
 
-parser.add_argument( "--where_to_place_molecule",     type=str, default="center")  # center or lower_corner
-
 parser.add_argument( "--pdb_out",     type=str, default="protein.pdb")
 parser.add_argument( "--box_out",     type=str, default="box.pdb")
 
@@ -39,14 +34,11 @@ counts = (args.x_count, args.y_count, args.z_count)
 
 pot_grid = PotentialGrid(args.prmtop_file, args.inpcrd_file, args.grid_nc,
                          new_calculation=True,
-                         lj_sigma_scaling_factor=args.lj_sigma_scaling_factor,
-                         lj_depth_scaling_factor=args.lj_depth_scaling_factor,
                          ionic_strength=args.ionic_strength,
                          dielectric=args.dielectric,
                          temperature=args.temperature,
                          spacing=args.spacing,
-                         counts=counts,
-                         where_to_place_molecule=args.where_to_place_molecule)
+                         counts=counts)
 
 pot_grid.write_pdb(args.pdb_out, "w")
 pot_grid.write_box(args.box_out)
